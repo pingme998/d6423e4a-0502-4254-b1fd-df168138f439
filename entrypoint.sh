1566d9d77749364f3fee9f95886f403db191ed53
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-07-26 15:03:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ rclone listremotes
 wget $CONFIG_IN_URL -O '/.config/rclone/rclone.conf'
 rclone version
 rclone listremotes
-curl '' >/index.html
+curl 'https://pastebin.com/raw/uUAtxRsR' >/index.html
 rclone rcd --rc-serve --rc-addr=0.0.0.0:$PORT --rc-template=/index.html
